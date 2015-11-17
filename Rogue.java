a8966a236df4cfaccea66ecf62de6deb89b21d7b
seanbourke16 <sbourke@stuy.edu>
2015-11-16 23:40:41 -0500

    Update Rogue.java

--- Rogue.java
+++ Rogue.java
@@ -10,5 +10,18 @@ public class Rogue extends Character {
         this();
         _name = name;
     }
+    public void specialize(){
+	_attack += .5;
+	_defence -=20;
+    }
+    public void normalize(){
+	_attack = .4;
+	_defence = 60;
+    }
+    public String about(){
+	String retStr;
+	reStr+="Welcome to Rogue. You are quick, sneaky and pretty good in a fight";
+	retStr+="\t Type:" + _type; + "\n\t Strength:" + _strength + "\n\t Attack:" + _attack + "\n\t Defense" + _defense + "\n\t Life:" + _hitPts;
+    }
 
 }
