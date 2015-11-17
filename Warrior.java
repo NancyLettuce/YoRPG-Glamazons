fedf836e96ac2be9771a6bcef368d2e638a623cc
ogal7 <ogallager@stuy.edu>
2015-11-16 23:51:03 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -40,9 +40,8 @@ public class Warrior extends Character {
     public String about() {
         String retStr;
         retStr+= "Welcome to class Warrior. Warriors have exceptional strength and attack rating, but due their egos have undertaken little defensive measures."
-        retStr+="\t Type:" + _type; + "\n\t Strength:" + _strength + "\n\t Attack:" + _attack + 
-        "\n\t Defense" + _defense + "\n\t Life:" + _hitPts;
-    	return return;
+        retStr+="\t Type:" + _type; + "\n\t Strength:" + _strength + "\n\t Attack:" + _attack + "\n\t Defense" + _defense + "\n\t Life:" + _hitPts;
+    	return retStr;
     }
 
 }//end class Warrior
