4a1e52926d48323c8101462b3ca8f6c7eb846361
NancyLettuce <nli4@stuy.edu>
2015-11-16 23:08:55 -0500

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,95 @@
+/*=============================================
+  class Character -- superclass for combatants in Ye Olde RPG
+  =============================================*/
+/*
+1. Make sure these methods belong to the Character superclass:
+isAlive X
+getName X
+getDefense X
+lowerHP X
+attack X
+normalize X
+specialize X
+about - static, returns a String describing a Warrior, Monster, Mage, Rogue, etc
+*/  
+public class Character {
+
+    // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+    protected int _hitPts;
+    protected int _strength;
+    protected int _defense;
+    protected double _attack;
+    protected String _name;
+    protected static String _type;
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /*=============================================
+      default constructor
+      pre:  instance vars are declared
+      post: initializes instance vars.
+      =============================================*/
+    public Character() {
+      _hitPts = 125;
+      _strength = 100;
+      _defense = 40;
+      _attack = .4;
+    }
+
+
+    public String getName() { 
+      return _name; 
+      
+    }
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+    public int getDefense() { 
+      return _defense; 
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /*=============================================
+      boolean isAlive() -- tell whether I am alive
+      post: returns boolean indicated alive or dead
+      =============================================*/
+    public boolean isAlive() {
+      return _hitPts > 0;
+    }
+
+    public void specialize() {
+      _attack *= 2;
+      _defense *= .5;
+    }
+
+    //revert to normal mode
+    public void normalize() {
+      _attack *= .5;
+     _defense *= 2;
+    }
+    /*=============================================
+      int attack(Character) -- simulates attack on a Monster
+      pre:  Input not null
+      post: Calculates damage to be inflicted, flooring at 0. 
+            Calls opponent's lowerHP() method to inflict damage. 
+      Returns damage dealt.
+      =============================================*/
+    public int attack( Character opponent ) {
+       int damage = (int)( (_strength * _attack) - opponent.getDefense() );
+       //System.out.println( "\t\t**DIAG** damage: " + damage );
+       if ( damage < 0 )
+           damage = 0;
+       opponent.lowerHP( damage );
+       return damage;
+    }//end attack
+
+
+    public static String about () {
+      return _type;//types will all be initialized in constructors of type classes, not reinitialized bc static
+    }
+
+    /*=============================================
+      void lowerHP(int) -- lowers life by input value
+      pre:  Input >= 0
+      post: Life instance var is lowered by input ammount.
+      =============================================*/
+    public void lowerHP( int damageInflicted ) {
+        _hitPts = _hitPts - damageInflicted;
+    }
+}//end class Character
