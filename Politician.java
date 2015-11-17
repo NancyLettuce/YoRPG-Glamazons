b28026e79b20fe59b56ad9a9dde73a87f9d544cc
ogal7 <ogallager@stuy.edu>
2015-11-16 23:23:51 -0500

    overwrote abstract methods

--- Politician.java
+++ Politician.java
@@ -4,11 +4,24 @@ public class Politician extends Character {
     public Politician() {
         super();
         _type="Politician";
-    	_strength = 400;//strong voices
+    	_strength = 250;//strong voices
     	_defense = 20;//anti gun laws means no hypocrisy right?
     	_attack = .5;
     	_name= "swag";
 	    
+    }
+    public void normalize() {
+    	_attack*=.66;
+    	_defense*=1.33;
+    }
+    public void specialize() {
+    	_attack*=1.5;
+    	_defense*=.75;
+    	
+    }
+    
+    public String about() {
+    	return "Welcome to class Politician! Your strong voice and the power you hold means you have the most strength of any character \n Yet, your opposition to guns in this game mean you lack defense. #feelthebern";
     }
     public Politician(String name) {
         this();
