115519129e97a077c218fb5310db353b035e5a2b
ogal7 <ogallager@stuy.edu>
2015-11-16 23:53:20 -0500

    Update Rogue.java

--- Rogue.java
+++ Rogue.java
@@ -20,7 +20,7 @@ public class Rogue extends Character {
     }
     public String about(){
 	String retStr;
-	reStr+="Welcome to Rogue. You are quick, sneaky and pretty good in a fight";
+	retStr+="Welcome to Rogue. You are quick, sneaky and pretty good in a fight";
 	retStr+="\t Type:" + _type + "\n\t Strength:" + _strength + "\n\t Attack:" + _attack + "\n\t Defense" + _defense + "\n\t Life:" + _hitPts;
 	return retStr;
     }
