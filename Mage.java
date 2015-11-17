7c6101136c26126bcb170aa60c2891f354cd65ad
NancyLettuce <nli4@stuy.edu>
2015-11-16 23:33:50 -0500

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -1,6 +1,7 @@
 public class Mage extends Character {
     protected static String _type;
     
+    //default constructor
     public Mage() {
         super();
         _type="Mage";
@@ -8,9 +9,29 @@ public class Mage extends Character {
     	_strength = 50;
 	    _defense = 100;//underdog 
     }
+    
+    //overloaded constructor
     public Mage(String name) {
         this();
         _name = name;
     }
-
+    
+    public void normalize() {
+    	attack = .4;
+    	defense = 100;
+    }
+    
+    public void specialize() {
+    	attack *= 2;
+    	defense *= .5;
+    }
+    
+    public String about() {
+    	return "Character: Mage \n
+    	HP = 125 \n
+    	Strength = 50 \n
+    	Defense = 100 \n
+    	Attack = .4 \n"
+    }
+    
 }
